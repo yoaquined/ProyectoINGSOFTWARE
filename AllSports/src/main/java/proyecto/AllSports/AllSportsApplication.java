2172e6c4d3fd6d81765aa416674e3d02297b434d
yoaquined <105896642+yoaquined@users.noreply.github.com>
2022-10-27 17:45:41 -0300

    Add files via upload

---
+++ AllSports/src/main/java/proyecto/AllSports/AllSportsApplication.java
@@ -0,0 +1,13 @@
+package proyecto.AllSports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllSportsApplication.class, args);
+	}
+
+}
---
+++ AllSports/src/test/java/proyecto/AllSports/AllSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.AllSports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
